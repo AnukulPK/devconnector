be9900604bc508d9085d024beb87d1357f778ede
Anukul <sspl-lap-064@Admins-MacBook-Pro-3.local>
2022-08-09 15:13:17 +0530

    added delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,21 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route  DELETE api/profile
+// @desc   Delete profile,user and post
+// @access Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    //Remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User deleted" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
