b6a5e3adb3e76b2afefe15fd68d4314db438cb47
unknown <anukulpk2@gmail.com>
2022-08-22 10:19:23 +0530

    React router dom added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Landing from "./components/layout/Landing";
 import NavBar from "./components/layout/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Landing />
-    </>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 };
 
